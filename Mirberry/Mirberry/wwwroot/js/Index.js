752a5c23338bf2c1023ab3e8371f7592efdccb85
James <jm.allen1@outlook.com>
2018-05-16 09:21:58 +0100

    Adding basic show/hide js
    Signed-off-by: James <jm.allen1@outlook.com>

---
+++ Mirberry/Mirberry/wwwroot/js/Index.js
@@ -0,0 +1,11 @@
+﻿var homeBanner = document.getElementById("banner-home");
+var beginBanner = document.getElementById("banner-begin");
+
+
+beginBanner.hidden = true;
+
+var beginButton = document.getElementById("begin-button");
+beginButton.addEventListener("click", function () {
+    beginBanner.hidden = false;
+    homeBanner.hidden = true;
+})
